0f6f8cddf31ea4df69beb001d3c01d07b16e2fdc
alexisBoutet <alexisBoutet>
2019-01-13 12:32:21 +0100

    Ajout de la possibilité de jouer

--- interface.py
+++ interface.py
@@ -40,7 +40,81 @@ def afficheDecision(dungeon, objects):
                 continuer = 0      #On arrête la boucle
     
         pygame.display.flip()
+ 
+def jouer(dungeon):
+    pygame.init()
+    tailleX = 60
+    tailleY = 36
+    fenetre = pygame.display.set_mode((len(dungeon.cases) * tailleX, len(dungeon.cases[0]) * tailleY))
+    perso = pygame.image.load("perso.png").convert_alpha()
+    image = [[0 for i in ligne] for ligne in dungeon.cases]
+    for i in range(len(dungeon.cases)):
+        for j in range(len(dungeon.cases[0])): 
+            case = dungeon.cases[i][j]
+            image[i][j] = pygame.image.load(case.image).convert()
+            fenetre.blit(image[i][j], (j*tailleX,i*tailleY))
             
+    
+    i, j = dungeon.startingPosition.i, dungeon.startingPosition.j
+    fenetre.blit(perso, (j*tailleX,i*tailleY))
+    adventurer = dungeon.adventurer
+    adventurer.goIn(dungeon.startingPosition)
+    adventurer.objects = []
+    state = dungeon.getState(dungeon.startingPosition, adventurer.objects)
+    
+    continuer = 1
+    #Boucle infinie
+    while continuer:
+        pygame.time.Clock().tick(30)
+        for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+            if event.type == QUIT:     #Si un de ces événements est de type QUIT
+                continuer = 0      #On arrête la boucle
+                
+            
+                
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    if "left" in case.voisin.keys():
+                        case = case.voisin["left"]
+                if event.key == K_RIGHT:
+                    if "right" in case.voisin.keys():
+                        case = case.voisin["right"]                 
+                if event.key == K_UP:
+                    if "top" in case.voisin.keys():
+                        case = case.voisin["top"]                 
+                if event.key == K_DOWN:
+                    if "bottom" in case.voisin.keys():
+                        case = case.voisin["bottom"]   
+                
+                # Si on souhaite avoir de l'aide
+                if event.key == K_SPACE:
+                    if state.decision:
+                        case = case.voisin[state.decision]
+                        
+                adventurer.goIn(case)
+                alive = adventurer.case.action(adventurer)
+                if not alive:
+                    adventurer.goIn(dungeon.startingPosition)
+                    adventurer.objects = []
+ 
+                case = adventurer.case
+                # Evenement de la pièce
+                state = dungeon.getState(case, adventurer.objects)
+                for i in range(len(dungeon.cases)):
+                    for j in range(len(dungeon.cases[0])):
+                        fenetre.blit(image[i][j], (j*tailleX,i*tailleY))
+            
+            
+            
+                    """ Quel état on est """
+                    #state.afficher()
+
+            
+                    #print(state.objects)
+                fenetre.blit(perso, (state.case.j*tailleX,state.case.i*tailleY))
+        #Rafraîchissement de l'écran
+        pygame.display.flip()
+           
 def affiche(dungeon):
     pygame.init()
     tailleX = 60
@@ -89,11 +163,7 @@ def affiche(dungeon):
             
                     """ Quel état on est """
                     #state.afficher()
-            
-            
-            
-            
-            
+
             
                     #print(state.objects)
                     fenetre.blit(perso, (state.case.j*tailleX,state.case.i*tailleY))
@@ -106,4 +176,4 @@ dungeon.instanciation(Adventurer(), False)
 #qlearning(dungeon)
 #valueIteration(dungeon)
 valueIteration(dungeon)
-afficheDecision(dungeon, [])
+jouer(dungeon)
